069ff0e524939a263c59dbb42d0173e683ccbc38
Gonçalo Marinho <a90969@alunos.uminho.pt>
2023-11-04 20:40:36 +0000

    outro fix

--- untitled/src/server.sh
+++ untitled/src/server.sh
@@ -2,4 +2,4 @@
 
 cd /../home/core/Desktop/CC/untitled/src
 
-javac Server/Main.java Server/FS_Tracker.java Server/FS_Track_Protocol.java Server/ClientHandler cmd/Chunk.java
+javac Server/Main.java Server/FS_Tracker.java Server/FS_Track_Protocol.java Server/ClientHandler.java cmd/Chunk.java
