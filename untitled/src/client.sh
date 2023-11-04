561245285a4a176f67de307c81bbcbce8de87a5f
Gonçalo Marinho <a90969@alunos.uminho.pt>
2023-11-04 20:38:27 +0000

    fix scripts

--- untitled/src/client.sh
+++ untitled/src/client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /../home/core/Desktop/CC/untitled1/src
+cd /../home/core/Desktop/CC/untitled/src
 
 javac Client/Main.java Client/FS_Node.java Client/ServerSend.java Client/ServerReceive.java Client/NodeSend.java cmd/Chunk.java
--- untitled/src/server.sh
+++ untitled/src/server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /../home/core/Desktop/CC/untitled1/src
+cd /../home/core/Desktop/CC/untitled/src
 
 javac Server/Main.java Server/FS_Tracker.java Server/FS_Track_Protocol.java Server/ClientHandler cmd/Chunk.java
